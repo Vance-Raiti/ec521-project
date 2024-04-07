e096a8e2f635ef9b931d8b306467c08c044f6e8c
Vance-Raiti <vraiti77@gmail.com>
2024-04-07 18:02:11 -0400

    retain all phishtank data

---
+++ scripts/get-dataset.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ ! -f online-valid.csv ]; then
+	wget http://data.phishtank.com/data/online-valid.csv.gz
+	gzip -d online-valid.csv.gz
+fi
+
+./filter-dataset.py 2>/dev/null
+
+cat urls/header.txt urls/urls*.txt > urls.txt

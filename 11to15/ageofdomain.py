03747cd90eeb69e1be1b9166b660b178554abeb6
Sebastian Gilligan <113646140+sjgilligan@users.noreply.github.com>
2024-04-17 17:48:55 -0400

    updated for compatibility

--- 11to15/ageofdomain.py
+++ 11to15/ageofdomain.py
@@ -1,7 +1,7 @@
 import whois
 from datetime import datetime
 
-def get_domain_age(url):
+def ageofdomain(url,html):
     try:
         domain_info = whois.whois(url)
         creation_date = domain_info.creation_date
@@ -17,18 +17,3 @@ def get_domain_age(url):
         print("Error:", e)
         return None
 
-def main():
-    url = input("Enter the URL to lookup: ")
-    age = get_domain_age(url)
-    if age is not None:
-        print(f"The domain '{url}' is {age} days old.")
-        if age < 30:
-            print("phishy")
-        else:
-            print("not phishy")
-    else:
-        print("Unable to retrieve domain age.")
-
-
-if __name__ == "__main__":
-    main()

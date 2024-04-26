2128980b2448d22db5563889aacdb1e3c6397440
Vance-Raiti <vraiti77@gmail.com>
2024-04-26 11:31:24 -0400

    consolidating network code

--- html_token_clf.py
+++ html_token_clf.py
@@ -9,55 +9,23 @@ from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace
 from parse import parse
 
-from sklearn.linear_model import LogisticRegression
-from tokenizers import Tokenizer
-from datasets import UrlDataset
-import pickle
+from bs4 import BeautifulSoup as bs
 
 VOCAB_SIZE = 200
 
 class UrlTokenClassifier:
 	def __init__(self):
-		if not exists('url-tokenizer.json'):
+		if not exists('html-tokenizer.json'):
 			self.train_tokenizer()	
-		self.tokenizer = Tokenizer.from_file('url-tokenizer.json')
-
-		if not exists('url-classifier.pkl'):
-			self.train_classifier()
-		with open("url-classifier.pkl","rb") as fp:
-			self.classifier = pickle.load(fp)
-
+		self.tokenizer = Tokenizer.from_file('html-tokenizer.json')
 
 	def train_tokenizer(self):
-		urls = [url for url,label in UrlDataset()]
+		htmls = [html for html,label in HtmlDataset()]
 		tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
 		tokenizer.pre_tokenizer = Whitespace()
 		trainer = BpeTrainer(vocab_size=VOCAB_SIZE,special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
-		tokenizer.train_from_iterator(urls,trainer)
-		tokenizer.save("url-tokenizer.json")
-
-	def block(self,data):
-		X = numpy.concatenate(
-			[
-				self.encode(url)
-				for url,_ in data
-			]
-		)
-		y = numpy.array([ label for _,label in data]) 
-		return X, y
-			
-		
-	def train_classifier(self):
-		data = UrlDataset(use_eps=False)
-		self.classifier = LogisticRegression()
-		print("training token classifier...")
-		self.classifier.fit(*self.block(data))
-		print("scoring token classifier...")
-		data.valid()
-		score = self.classifier.score(*self.block(data))
-		print(f"url classifier has mean score {score}")
-		with open("url-classifier.pkl","wb") as fp:
-			pickle.dump(self.classifier,fp)
+		tokenizer.train_from_iterator(htmls,trainer)
+		tokenizer.save("html-tokenizer.json")
 
 	def encode(self,string):
 		string = string.split(":")
